93c9f59b6b72bdbaa43ad5e0e04ac1df49eafe88
AlexG9917 <160983963+AlexG9917@users.noreply.github.com>
2024-10-25 17:21:50 -0400

    Add files via upload

---
+++ Form1mio.Designer.vb
@@ -0,0 +1,457 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.Button19 = New System.Windows.Forms.Button()
+        Me.Button20 = New System.Windows.Forms.Button()
+        Me.Button21 = New System.Windows.Forms.Button()
+        Me.Button22 = New System.Windows.Forms.Button()
+        Me.Button23 = New System.Windows.Forms.Button()
+        Me.Button24 = New System.Windows.Forms.Button()
+        Me.Button25 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ButtonAyuda = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label
+        '
+        Me.Label.AutoSize = True
+        Me.Label.BackColor = System.Drawing.Color.Gainsboro
+        Me.Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label.Font = New System.Drawing.Font("Lucida Calligraphy", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label.Location = New System.Drawing.Point(39, 155)
+        Me.Label.Name = "Label"
+        Me.Label.Size = New System.Drawing.Size(331, 19)
+        Me.Label.TabIndex = 2
+        Me.Label.Text = "Convertir unidades de medida Distancia"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.DodgerBlue
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(339, 90)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(134, 20)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Text = "Ingresar Valor"
+        '
+        'Button1
+        '
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Location = New System.Drawing.Point(294, 245)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(85, 40)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "(m) a (Cm)"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(339, 376)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(134, 20)
+        Me.TextBox2.TabIndex = 5
+        Me.TextBox2.Text = "Resultado"
+        '
+        'Button2
+        '
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.Location = New System.Drawing.Point(25, 245)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(83, 40)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "(Km) a (Cm)"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button3.Location = New System.Drawing.Point(114, 291)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(85, 40)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "(Cm) a (mm)"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Crimson
+        Me.Button4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Button4.Location = New System.Drawing.Point(632, 392)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(171, 61)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "Salir"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button5.Location = New System.Drawing.Point(114, 199)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(83, 40)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "(Cm) a (Km)"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button6.Location = New System.Drawing.Point(114, 245)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(85, 40)
+        Me.Button6.TabIndex = 10
+        Me.Button6.Text = "(Cm) a (m)"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button7.Location = New System.Drawing.Point(203, 290)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(85, 41)
+        Me.Button7.TabIndex = 11
+        Me.Button7.Text = "(mm) a (Cm)"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button8.Location = New System.Drawing.Point(25, 199)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(83, 40)
+        Me.Button8.TabIndex = 12
+        Me.Button8.Text = "(Km) a (m)"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button9.Location = New System.Drawing.Point(25, 291)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(83, 40)
+        Me.Button9.TabIndex = 13
+        Me.Button9.Text = "(Km) a (mm)"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button10.Location = New System.Drawing.Point(203, 199)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(85, 40)
+        Me.Button10.TabIndex = 14
+        Me.Button10.Text = "(mm) a (Km)"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button11.Location = New System.Drawing.Point(203, 245)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(85, 40)
+        Me.Button11.TabIndex = 15
+        Me.Button11.Text = "(mm) a (m)"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button12.Location = New System.Drawing.Point(294, 199)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(85, 40)
+        Me.Button12.TabIndex = 16
+        Me.Button12.Text = "(m) a (Km)"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button13.Location = New System.Drawing.Point(294, 291)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(85, 40)
+        Me.Button13.TabIndex = 17
+        Me.Button13.Text = "(m) a (mm)"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button14.Location = New System.Drawing.Point(697, 291)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(85, 40)
+        Me.Button14.TabIndex = 29
+        Me.Button14.Text = "(mg) a (cg)"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button15.Location = New System.Drawing.Point(697, 199)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(85, 40)
+        Me.Button15.TabIndex = 28
+        Me.Button15.Text = "(mg) a (Kg)"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button16.Location = New System.Drawing.Point(606, 245)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(85, 40)
+        Me.Button16.TabIndex = 27
+        Me.Button16.Text = "(cg) a (g)"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button17.Location = New System.Drawing.Point(606, 199)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(85, 40)
+        Me.Button17.TabIndex = 26
+        Me.Button17.Text = "(cg) a (Kg)"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Button18
+        '
+        Me.Button18.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button18.Location = New System.Drawing.Point(428, 291)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(83, 40)
+        Me.Button18.TabIndex = 25
+        Me.Button18.Text = "(Kg) a (mg)"
+        Me.Button18.UseVisualStyleBackColor = True
+        '
+        'Button19
+        '
+        Me.Button19.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button19.Location = New System.Drawing.Point(428, 199)
+        Me.Button19.Name = "Button19"
+        Me.Button19.Size = New System.Drawing.Size(83, 40)
+        Me.Button19.TabIndex = 24
+        Me.Button19.Text = "(Kg) a (g)"
+        Me.Button19.UseVisualStyleBackColor = True
+        '
+        'Button20
+        '
+        Me.Button20.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button20.Location = New System.Drawing.Point(606, 290)
+        Me.Button20.Name = "Button20"
+        Me.Button20.Size = New System.Drawing.Size(85, 41)
+        Me.Button20.TabIndex = 23
+        Me.Button20.Text = "(cg) a (mg)"
+        Me.Button20.UseVisualStyleBackColor = True
+        '
+        'Button21
+        '
+        Me.Button21.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button21.Location = New System.Drawing.Point(517, 245)
+        Me.Button21.Name = "Button21"
+        Me.Button21.Size = New System.Drawing.Size(85, 40)
+        Me.Button21.TabIndex = 22
+        Me.Button21.Text = "(g) a (cg)"
+        Me.Button21.UseVisualStyleBackColor = True
+        '
+        'Button22
+        '
+        Me.Button22.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button22.Location = New System.Drawing.Point(517, 199)
+        Me.Button22.Name = "Button22"
+        Me.Button22.Size = New System.Drawing.Size(83, 40)
+        Me.Button22.TabIndex = 21
+        Me.Button22.Text = "(g) a (Kg)"
+        Me.Button22.UseVisualStyleBackColor = True
+        '
+        'Button23
+        '
+        Me.Button23.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button23.Location = New System.Drawing.Point(517, 291)
+        Me.Button23.Name = "Button23"
+        Me.Button23.Size = New System.Drawing.Size(85, 40)
+        Me.Button23.TabIndex = 20
+        Me.Button23.Text = "(g) a (mg)"
+        Me.Button23.UseVisualStyleBackColor = True
+        '
+        'Button24
+        '
+        Me.Button24.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button24.Location = New System.Drawing.Point(428, 245)
+        Me.Button24.Name = "Button24"
+        Me.Button24.Size = New System.Drawing.Size(83, 40)
+        Me.Button24.TabIndex = 19
+        Me.Button24.Text = "(Kg) a (cg)"
+        Me.Button24.UseVisualStyleBackColor = True
+        '
+        'Button25
+        '
+        Me.Button25.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button25.Location = New System.Drawing.Point(697, 245)
+        Me.Button25.Name = "Button25"
+        Me.Button25.Size = New System.Drawing.Size(85, 40)
+        Me.Button25.TabIndex = 18
+        Me.Button25.Text = "(mg) a (g)"
+        Me.Button25.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Gainsboro
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Font = New System.Drawing.Font("Lucida Calligraphy", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(458, 155)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(286, 19)
+        Me.Label1.TabIndex = 30
+        Me.Label1.Text = "Convertir unidades de media Masa"
+        '
+        'ButtonAyuda
+        '
+        Me.ButtonAyuda.BackColor = System.Drawing.Color.DarkOrange
+        Me.ButtonAyuda.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonAyuda.Location = New System.Drawing.Point(0, 392)
+        Me.ButtonAyuda.Name = "ButtonAyuda"
+        Me.ButtonAyuda.Size = New System.Drawing.Size(152, 61)
+        Me.ButtonAyuda.TabIndex = 31
+        Me.ButtonAyuda.Text = "Ayuda"
+        Me.ButtonAyuda.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label2.Font = New System.Drawing.Font("Monotype Corsiva", 18.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Gold
+        Me.Label2.Image = Global.Convertidor.My.Resources.Resources.un_fondo_para_una_app_que_represente_las_unidades_de_medida_de_distancia_y_masa
+        Me.Label2.Location = New System.Drawing.Point(453, 19)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(319, 30)
+        Me.Label2.TabIndex = 32
+        Me.Label2.Text = "Alexander Gonzalez : 29809917"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Convertidor.My.Resources.Resources.un_fondo_para_una_app_que_represente_las_unidades_de_medida_de_distancia_y_masa
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ButtonAyuda)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.Button18)
+        Me.Controls.Add(Me.Button19)
+        Me.Controls.Add(Me.Button20)
+        Me.Controls.Add(Me.Button21)
+        Me.Controls.Add(Me.Button22)
+        Me.Controls.Add(Me.Button23)
+        Me.Controls.Add(Me.Button24)
+        Me.Controls.Add(Me.Button25)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Name = "Form1"
+        Me.Text = "Convertidor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents Button20 As Button
+    Friend WithEvents Button21 As Button
+    Friend WithEvents Button22 As Button
+    Friend WithEvents Button23 As Button
+    Friend WithEvents Button24 As Button
+    Friend WithEvents Button25 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ButtonAyuda As Button
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Form1mio.vb
@@ -0,0 +1,185 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Public Property TextBox1 As Object
+        Public Property TextBox2 As Object
+
+        Private Sub InitializeComponent()
+            Throw New NotImplementedException()
+        End Sub
+
+    End Class
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim Distancia As Double = (Cantidad * 100)
+        Me.TextBox2.Text = Distancia.ToString()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim Distancia As Single = (Cantidad * 100000)
+        Me.TextBox2.Text = Distancia.ToString()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim Distancia As Single = (Cantidad * 1000)
+        Me.TextBox2.Text = Distancia.ToString()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim Distancia As Single = (Cantidad / 1000)
+        Me.TextBox2.Text = Distancia.ToString()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim Distancia As Single = (Cantidad / 100)
+        Me.TextBox2.Text = Distancia.ToString()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim Distancia As Single = (Cantidad / 10)
+        Me.TextBox2.Text = Distancia.ToString()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim Distancia As Single = (Cantidad * 1000)
+        Me.TextBox2.Text = Distancia.ToString()
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim Distancia As Single = (Cantidad * 1000000)
+        Me.TextBox2.Text = Distancia.ToString()
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim Distancia As Single = (Cantidad / 100000)
+        Me.TextBox2.Text = Distancia.ToString()
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim Distancia As Single = (Cantidad / 1000)
+        Me.TextBox2.Text = Distancia.ToString()
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim Distancia As Single = (Cantidad / 1000)
+        Me.TextBox2.Text = Distancia.ToString()
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim Distancia As Single = (Cantidad * 100)
+        Me.TextBox2.Text = Distancia.ToString()
+    End Sub
+
+    Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Button19.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim masa As Single = (Cantidad * 1000)
+        Me.TextBox2.Text = masa.ToString()
+    End Sub
+
+    Private Sub Button24_Click(sender As Object, e As EventArgs) Handles Button24.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim masa As Single = (Cantidad * 100000)
+        Me.TextBox2.Text = masa.ToString()
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim masa As Single = (Cantidad * 1000000)
+        Me.TextBox2.Text = masa.ToString()
+    End Sub
+
+    Private Sub Button22_Click(sender As Object, e As EventArgs) Handles Button22.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim masa As Single = (Cantidad / 100)
+        Me.TextBox2.Text = masa.ToString()
+    End Sub
+
+    Private Sub Button21_Click(sender As Object, e As EventArgs) Handles Button21.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim masa As Single = (Cantidad * 100)
+        Me.TextBox2.Text = masa.ToString()
+    End Sub
+
+    Private Sub Button23_Click(sender As Object, e As EventArgs) Handles Button23.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim masa As Single = (Cantidad * 1000)
+        Me.TextBox2.Text = masa.ToString()
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim masa As Single = (Cantidad / 100000)
+        Me.TextBox2.Text = masa.ToString()
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim masa As Single = (Cantidad / 100)
+        Me.TextBox2.Text = masa.ToString()
+    End Sub
+
+    Private Sub Button20_Click(sender As Object, e As EventArgs) Handles Button20.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim masa As Single = (Cantidad * 10)
+        Me.TextBox2.Text = masa.ToString()
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim masa As Single = (Cantidad / 1000000)
+        Me.TextBox2.Text = masa.ToString()
+    End Sub
+
+    Private Sub Button25_Click(sender As Object, e As EventArgs) Handles Button25.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim masa As Single = (Cantidad / 1000)
+        Me.TextBox2.Text = masa.ToString()
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        Dim Cantidad As Single = Single.Parse(Me.TextBox1.Text)
+        Dim masa As Single = (Cantidad / 10)
+        Me.TextBox2.Text = masa.ToString()
+    End Sub
+
+    Private Sub ButtonAyuda_Click(sender As Object, e As EventArgs) Handles ButtonAyuda.Click
+        MessageBox.Show("Este programa convierte unidades de peso de la siguiente manera." &
+             " 
+             Ejemplo: 
+             " &
+             "
+             Para convertir de kg a g, ingrese el valor en kg y haga clic en el botón (Kg) a (g). " &
+             "
+             Para convertir de g a kg, ingrese el valor en g y haga clic en el botón (g) a (Kg).",
+             "
+             El resultado se mostrara automaticamente en el Textbox inferior ", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+End Class
